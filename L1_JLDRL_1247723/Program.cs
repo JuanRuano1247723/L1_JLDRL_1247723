8cb71c3cdb56f28af1d5d5d393696c59aa7a2018
04dan <04dan@LAPTOP-35D3RKBD>
2023-02-18 11:28:10 -0600

    Agregar archivos de proyecto.

---
+++ L1_JLDRL_1247723/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace L1_JLDRL_1247723
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo soy Ruano");
+                Console.ReadKey();
+
+            Console.WriteLine();
+                Console.WriteLine("Hola mundo");
+                Console.WriteLine("soy Ruano");
+            /*Cambia de línea al finalizar la sentencia*/
+            Console.WriteLine();
+                Console.Write("Hola Mundo");
+                Console.Write(" soy Ruano");
+            /*Lo escribe todo seguido, puede ser reemplazada si se escribe de forma seguida otra línea*/
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese su nombre: ");
+                String NOMBRE = Console.ReadLine();
+                Console.ReadKey();
+
+            Console.WriteLine();
+                Console.WriteLine("Hola mundo");
+                Console.WriteLine("soy "+NOMBRE);
+            /*Cambia de línea al finalizar la sentencia*/
+            Console.WriteLine();
+                Console.Write("Hola Mundo");
+                Console.Write(" soy "+NOMBRE);
+            /*Lo escribe todo seguido*/
+                Console.ReadKey();
+
+
+
+        }
+    }
+}
